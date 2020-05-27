cb6eb0904d9b95ef31af171cb7a00c9311421854
yehias21 <62369984+yehias21@users.noreply.github.com>
2020-05-27 20:21:29 +0200

    Add files via upload
    under construction

--- main.c
+++ main.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+typedef union
+{
+    double dData;
+    char  cData;
+} Item;
+
+typedef struct
+{
+    Item* data;
+    struct node * next;
+
+} node;
+int character_match(char x,int y)//implement y  conditions
+{
+  const char symbols1[]="+-*/^(";
+   const char symbols1[]="+-*/^)";
+  int i=0;
+  while(sybols[i]!='\0')
+  {
+      if(sybols[i]==x)
+        return 1;
+      i++;
+  }
+  return 0;
+}
+
+double rightside_evaluation(char * buff)
+{ double num=0;
+
+checker_bracket(buff);
+return num;
+}
+int main()
+{
+  char buff[]="10.5+(z-2.5)/(x+y)";
+  double num=rightside_evaluation(buff);
 }
