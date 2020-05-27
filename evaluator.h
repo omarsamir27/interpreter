46a3e81b1977e67620afefe4a58c8ff07b45706e
yehias21 <62369984+yehias21@users.noreply.github.com>
2020-05-28 01:22:38 +0200

    Add files via upload

---
+++ evaluator.h
@@ -0,0 +1,7 @@
+
+#ifndef INTERPRETER_EVALUATOR_H
+#define INTERPRETER_EVALUATOR_H
+void err(int x,char * buff);
+void checker_bracket(char* buff);
+double rightside_evaluation(char * str);
+#endif // INTERPRETER_EVALUATOR_H
