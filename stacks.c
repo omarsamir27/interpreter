146e1e3eec0f092cd511bdde6f83036b686dff3f
yehias21 <62369984+yehias21@users.noreply.github.com>
2020-05-28 02:22:23 +0200

    Add files via upload

--- stacks.c
+++ stacks.c
@@ -3,45 +3,20 @@
 #include <string.h>
 #include<ctype.h>
 #include<math.h>
-typedef union
-{
-    double dData;
-    char  cData;
-} Item;
-
-typedef struct
-{
-    Item* data;
-    struct Stack * next;
-
-} Stack;
-
-
+#include "stacks.h"
 Stack * initialize()
 {
     Stack* newStack=malloc(sizeof(Stack));
     newStack->data=malloc(sizeof(Item));
     return newStack;
 }
-
 int isEmpty(Stack *s)
 {
     return s==NULL;
-
 }
-
 Item top(Stack **s)
 {
     return *((*s)->data);
-}
-void destroy(Stack **s)
-{
-    while(*s!=NULL)
-    Stack* temp;
-    temp=*s;
-    *s = (*s)->next;
-    free(temp->data);
-    free( temp );
 }
 Item  pop(Stack **s)
 {
@@ -54,7 +29,6 @@ Item  pop(Stack **s)
     free( temp );
     return popVal;
 }
-
 void push(Stack **s, Item* val)
 {
     Stack* newStack= initialize();
@@ -65,26 +39,86 @@ void push(Stack **s, Item* val)
 int precdOpr(char opr)
 {
     if(opr=='+'||opr=='-')
-    return 0 ;
+        return 0 ;
     else if(opr=='/'||opr=='*')
-    return 1;
+        return 1;
     else if(opr=='^')
-    return 2;
+        return 2;
     else
-    return -1;
-
+        return -1;
 }
-void infixToPostfix(char* infix, char* postfix)
+
+void operatorHandle(char opr,Stack**head)
 {
-char* token= strtok(infix," ");
-    Item temp;char tempOpr[2];tempOpr[1]='\0';
-    Stack *head=NULL;
+    Item op1=pop(head);
+    Item op2=pop(head);
+    switch(opr)
+    {
+    case '+' :
+        op1.dData=op2.dData+op1.dData;
+        push(head,&op1);
+        break;
+    case '-' :
+        op1.dData=op2.dData-op1.dData;
+        push(head,&op1);
+        break;
+    case '/' :
+        op1.dData=op2.dData/op1.dData;
+        push(head,&op1);
+        break;
+    case '*' :
+        op1.dData=op2.dData*op1.dData;
+        push(head,&op1);
+        break;
+    case '^' :
+        op1.dData=pow(op2.dData,op1.dData);
+         push(head,&op1);
+        break;
+    }
+}
+double evaluatePostfix(char* postfix)
+{
+    char* token= strtok(postfix," ");
+    Item temp;
+    Stack * head = NULL;
     while(token!= NULL)
     {
-        if(isalpha(token[0])||isdigit(token[0])||token[0]=='_')
+        if(isdigit(token[0])||(isdigit(token[1])))
         {
-           strcat(postfix,token);
-           strcat(postfix," ");
+            temp.dData= atof(token);
+            push(&head,&temp);
+        }
+        else if (isalpha(token[0])||token[0]==('_'))
+        {
+            temp.dData; //getkey;
+            push(&head,&temp);
+        }
+        else
+        {
+            operatorHandle(*token,&head);
+        }
+
+        token=strtok(NULL," ");
+    }
+    temp=pop(&head);
+    return temp.dData;
+
+}
+
+double infixToPostfix(char* infix)
+{   Stack * head = NULL;
+    char postfix[256];
+    char* token= strtok(infix," ");
+    Item temp;
+    char tempOpr[2];
+    tempOpr[1]='\0';
+    memset(postfix,0,256);
+    while(token!= NULL)
+    {
+        if(isdigit(token[0])||isalpha(token[0])||token[0]==('_')||(isdigit(token[1])))
+        {
+            strcat(postfix,token);
+            strcat(postfix," ");
         }
         else if(token[0]=='(')
         {
@@ -92,7 +126,8 @@ char* token= strtok(infix," ");
             push(&head,&temp);
         }
         else if (token[0]==')')
-        { temp= pop(&head);
+        {
+            temp= pop(&head);
 
             while(temp.cData!='(')
             {
@@ -107,7 +142,7 @@ char* token= strtok(infix," ");
         else
         {
             if(!isEmpty(head))
-            temp=top(&head);
+                temp=top(&head);
             while(!isEmpty(head)&&(precdOpr(token[0])<=precdOpr(temp.cData)))
             {
                 tempOpr[0]=temp.cData;
@@ -115,101 +150,24 @@ char* token= strtok(infix," ");
                 strcat(postfix," ");
                 pop(&head);
                 if(!isEmpty(head))
-                temp=top(&head);
+                    temp=top(&head);
 
             }
             temp.cData=token[0];
             push(&head,&temp);
         }
         token=strtok(NULL," ");
-    destroy(&head);
-}
 
-while(!isEmpty(head))
+    }
+
+    while(!isEmpty(head))
     {
         temp=pop(&head);
         tempOpr[0]=temp.cData;
         strcat(postfix,tempOpr);
         strcat(postfix," ");
     }
-
     postfix[strlen(postfix)-1]='\0';
-}
-void operatorHandle(char opr,Stack**head)
-{
-    Item op1=pop(&head);
-    Item op2=pop(&head);
-    switch(opr){
-    case '+' :
-       op1.dData=op2.dData+op1.dData;
-       push(&head,&op1);
-       break;
-    case '-' :
-      op1.dData=op2.dData-op1.dData;
-       push(&head,&op1);
-          break;
-    case '/' :
-      op1.dData=op2.dData/op1.dData;
-       push(&head,&op1);
-          break;
-    case '*' :
-        op1.dData=op2.dData*op1.dData;
-       push(&head,&op1);
-          break;
-    case '^' :
-   op1.dData=pow(op2.dData,op1.dData);
-       push(&head,&op1);
-          break;
-}}
-double evaluatePostfix(char* postfix)
-{
-    char* token= strtok(postfix," ");
-    Item temp;
-    Stack *head=NULL;
-    while(token!= NULL)
-    {
-        if(isdigit(token[0]))
-        {
-            temp.dData= atof(token);
-            push(&head,&temp);
-        }
-        else if (isalpha(token[0])||token[0]=='_')
-        {
-         // temp.dData= getkey(token);
-            push(&head,&temp);
-        }
-        else
-           {operatorHandle(*token,&head);}
-
-        token=strtok(NULL," ");
-    }
-    temp=pop(&head);
-    destroy(&head);
-    return temp.dData;
-
+    return evaluatePostfix(postfix);
 }
 
-void replaceNewLineBySpace(char *s)
-{
-    char *s1 = s;
-    while((s1 = strstr(s1, "\n")) != NULL)
-        *s1 = ' ';
-}
-
-int main(int argc, char**argv)
-{
-    char* infixExpr = malloc(sizeof(char)*256);
-    char* postfixExpr = malloc(sizeof(char)*256);
-    memset(postfixExpr,0,256);
-    printf("Enter an expression you want to evaluate or Ctrl+Z to exit: ");
-    while(fgets(infixExpr, 255, stdin) != NULL)
-    {
-        replaceNewLineBySpace(infixExpr);
-        infixToPostfix(infixExpr, postfixExpr);
-        printf("Postfix : %s\n", postfixExpr);
-        double result = evaluatePostfix(postfixExpr);
-        printf("Result: %.3f\n\n", result);
-        printf("Enter an expression you want to evaluate or Ctrl+Z to exit: ");
-    }
-    return 0;
-}
