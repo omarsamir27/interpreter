76bd03f8b83c1cdb377e6a162f2046f1b3ac5f2a
Omar Mohamed Samir <39497615+omarsamir27@users.noreply.github.com>
2020-06-16 14:16:01 +0200

    Update fileLoad.c

--- fileLoad.c
+++ fileLoad.c
@@ -14,6 +14,7 @@
 extern char* case_mode;
 heapnode* loadFile(char* filename,BST* bst) {
     FILE *file = fopen(filename, "r");
+    if(!file) fprintf(stderr,"File Does Not Exist");
     fseek(file, 0, SEEK_END);
     long file_size = ftell(file);
     rewind(file);
